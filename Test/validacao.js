5d2edfb6fea603029c70d99058225c10a09972ee
Quiteria <qteixeira@yahoo.com.br>
2023-06-06 20:17:55 -0300

    VersaoFinal

--- Test/validacao.js
+++ Test/validacao.js
@@ -1,6 +1,8 @@
 const assert = require('assert');
 const pessoas = require('../DesafioQuatro/desafioQuatro');
 
+//Pesquise uma forma de testar se o módulo está retornando a quantidade correta usando a bbiblioteca Mocha do Javascrit. 
+
 describe('Tamanho da lista de nomes', function () {
     it('deve ter 6 itens', function () {
         assert.strictEqual(pessoas.length, 6);
