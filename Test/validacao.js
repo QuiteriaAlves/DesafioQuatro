555f119d49e5e8fbf7fa306e47f9defc283f8e39
Quiteria <qteixeira@yahoo.com.br>
2023-06-05 23:04:19 -0300

    DesafioQuatro

--- Test/validacao.js
+++ Test/validacao.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
-//const pessoas = require('../Desafio/desafioQuatro');
+import { pessoas }  from "../DesafioQuatro/desafioQuatro";
 
 describe('Tamanho da lista de nomes', function () {
     it('deve ter 6 itens', function () {
-        const teste = [1, 2, 3, 4, 4, 5]
+        const teste = [1, 2, 3, 4, 4, 5];
         assert.strictEqual(teste.length, 6);
     });
 });
